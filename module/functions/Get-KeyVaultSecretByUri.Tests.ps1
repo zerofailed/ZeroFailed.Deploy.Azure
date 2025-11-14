aca4f3deb40b0f52856f139996faaeb4f29188bb
James Dawson <james.dawson@endjin.com>
2025-11-14 13:07:12 +0000

    Tweak mocking of Az.KeyVault command

--- module/functions/Get-KeyVaultSecretByUri.Tests.ps1
+++ module/functions/Get-KeyVaultSecretByUri.Tests.ps1
@@ -5,11 +5,11 @@
 BeforeAll {
     . $PSCommandPath.Replace('.Tests.ps1', '.ps1')
     
+    Import-Module Az.KeyVault
+
     # Mock external dependencies
     Mock Import-Module {}
-    
-    Import-Module Az.KeyVault
-    Mock Get-AzKeyVaultSecret {
+    Mock Get-AzKeyVaultSecret -ModuleName Az.KeyVault {
         # Return a mock SecureString
         ConvertTo-SecureString -String "mock-secret-value" -AsPlainText -Force
     }
